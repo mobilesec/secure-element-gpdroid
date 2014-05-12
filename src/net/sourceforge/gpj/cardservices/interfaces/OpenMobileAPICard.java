0420895b6ba7c4b97c1c99c58abe37b1cf418c79
michaelhoelzl <mh@usmile.at>
2014-05-12 19:13:13 +0200

    added license in header

--- src/net/sourceforge/gpj/cardservices/interfaces/OpenMobileAPICard.java
+++ src/net/sourceforge/gpj/cardservices/interfaces/OpenMobileAPICard.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Michael Hölzl <mihoelzl@gmail.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Michael Hölzl <mihoelzl@gmail.com> - initial implementation
+ ******************************************************************************/
 package net.sourceforge.gpj.cardservices.interfaces;
 
 import java.io.IOException;
